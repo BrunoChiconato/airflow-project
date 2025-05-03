08ee5169fd9813695222d9b3760334107e1a0cfd
Bruno Chiconato <brunochiconato01@gmail.com>
2025-05-03 11:50:55 -0300

    feat(stock_market): add format_prices task using DockerOperator with Spark
    - Integrate a new task, format_prices, into the DAG utilizing DockerOperator to execute the 'stock-app' image
    - This image contains the stock_transform module, which leverages Apache Spark to perform transformations on stock price data.

--- dags/stock_market.py
+++ dags/stock_market.py
@@ -2,6 +2,7 @@ from airflow.decorators import dag, task
 from airflow.sensors.base import PokeReturnValue
 from airflow.hooks.base import BaseHook
 from airflow.operators.python import PythonOperator
+from airflow.providers.docker.operators.docker import DockerOperator
 from datetime import datetime
 import requests
 
@@ -46,7 +47,22 @@ def stock_market():
         }
     )
 
-    is_api_available() >> get_stock_prices >> store_prices
+    format_prices = DockerOperator(
+        task_id = 'format_prices',
+        image = 'airflow/stock-app',
+        api_version = 'auto',
+        auto_remove = 'success',
+        docker_url = 'tcp://docker-proxy:2375',
+        network_mode="container:spark-master",
+        tty = True,
+        xcom_all = False,
+        mount_tmp_dir = False,
+        environment = {
+            'SPARK_APPLICATION_ARGS': '{{ ti.xcom_pull(task_ids="store_prices") }}'
+        }
+    )
+
+    is_api_available() >> get_stock_prices >> store_prices >> format_prices
 
 stock_market()
 
