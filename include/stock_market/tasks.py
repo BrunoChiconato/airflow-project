2a98fbb21de9ca7327ca9551caa2e72bcf7345fd
Bruno Chiconato <brunochiconato01@gmail.com>
2025-05-04 16:27:27 -0300

    refactor(tasks): use MinIO client helper and add CSV name formatter
    - Imported the MinIO client creation function into tasks.py and updated dependent functions accordingly
    - Also added a new _get_formatted_csv function to return the formatted name of a CSV file from a MinIO bucket

--- include/stock_market/tasks.py
+++ include/stock_market/tasks.py
@@ -1,8 +1,12 @@
 from airflow.hooks.base import BaseHook
-from minio import Minio
+from airflow.exceptions import AirflowNotFoundException
 from io import BytesIO
+from include.helpers.minio import get_minio_client
 import requests
 import json
+
+
+BUCKET_NAME = 'stock-market'
 
 def _get_stock_prices(url: str, symbol: str):
     url = f'{url}{symbol}?metrics=high?&interval=1d&range=1y'
@@ -14,27 +18,36 @@ def _get_stock_prices(url: str, symbol: str):
 
 
 def _store_prices(stock):
-    minio = BaseHook.get_connection('minio')
-    client = Minio(
-        endpoint = minio.extra_dejson['endpoint_url'].split('//')[1],
-        access_key = minio.login,
-        secret_key = minio.password,
-        secure = False
-    )
+    client = get_minio_client()
 
-    bucket_name = 'stock-market'
-    if not client.bucket_exists(bucket_name):
-        client.make_bucket(bucket_name)
+    if not client.bucket_exists(BUCKET_NAME):
+        client.make_bucket(BUCKET_NAME)
 
     stock = json.loads(stock)
     symbol = stock['meta']['symbol']
     data = json.dumps(stock, ensure_ascii=False).encode('utf-8')
 
     objw = client.put_object(
-        bucket_name,
+        BUCKET_NAME,
         object_name=f'{symbol}/prices.json',
         data=BytesIO(data),
         length=len(data)
     )
 
     return f'{objw.bucket_name}/{symbol}'
+
+
+def _get_formatted_csv(path: str):
+    client = get_minio_client()
+    prefix_name = f'{path.split('/')[1]}/formatted_prices/'
+    objects = client.list_objects(
+        BUCKET_NAME,
+        prefix_name,
+        recursive=True
+    )
+
+    for obj in objects:
+        if obj.object_name.endswith('.csv'):
+            return obj.object_name
+
+    return AirflowNotFoundException('The csv file does not exist.')
