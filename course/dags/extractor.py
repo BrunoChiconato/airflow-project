f82a6629a65c9429d6dcbdb3916018a51f5f7e44
Bruno Chiconato <brunochiconato01@gmail.com>
2025-05-07 18:19:13 -0300

    feat(dags): add 'check_size' task to demonstrate XCom push/pull
    - Adds a new task 'check_size' that pulls the size of the 'cocktail.json' file and checks its size, demonstrating the use of 'xcom_push'

--- course/dags/extractor.py
+++ course/dags/extractor.py
@@ -6,14 +6,21 @@ from pendulum import datetime
 import requests
 
 
-DATASET_COCKTAIL = Dataset('./tmp/cocktail.json')
+DATASET_COCKTAIL = Dataset('/tmp/cocktail.json')
 
-def _get_cocktail():
+def _get_cocktail(ti=None):
     api = 'https://www.thecocktaildb.com/api/json/v1/1/random.php'
     response = requests.get(api)
 
     with open(DATASET_COCKTAIL.uri, 'wb') as f:
         f.write(response.content)
+
+    ti.xcom_push(key='request_size', value=len(response.content))
+
+
+def _check_size(ti=None):
+    size = ti.xcom_pull(key='request_size', task_ids='get_cocktail')
+    print(size)
 
 
 @dag(
@@ -28,5 +35,12 @@ def extractor():
         python_callable = _get_cocktail,
         outlets = [DATASET_COCKTAIL]
     )
+
+    check_size = PythonOperator(
+        task_id = 'check_size',
+        python_callable = _check_size
+    )
+
+    get_cocktail >> check_size
 
 extractor()
